824c180e0631563182fb2911cd21810241e4fa12
Pivok <pivoc@protonmail.com>
2025-05-29 21:58:41 +0200

    Strip release binary

--- src/templates/zig/build.zig
+++ src/templates/zig/build.zig
@@ -1,9 +1,9 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+
     const exe = b.addExecutable(.{
         .name = "main",
         .root_source_file = b.path("src/main.zig"),
