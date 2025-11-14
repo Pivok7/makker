a92e34b3bf636cdc919eaa0ba0fc4858dceac54f
Pivok <pivoc@protonmail.com>
2025-11-14 23:23:28 +0100

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Dev flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-unstable,
+      ...
+    }:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      devShells."${system}".default =
+        let
+          pkgs = import nixpkgs { inherit system; };
+          pkgs-unstable = import nixpkgs-unstable { inherit system; };
+        in
+        pkgs.mkShell {
+          packages =
+            (with pkgs; [
+              nushell
+            ])
+            ++ (with pkgs-unstable; [
+              zig
+            ]);
+
+          shellHook = ''
+            echo "zig `zig version`"
+            exec nu
+          '';
+        };
+    };
+}
