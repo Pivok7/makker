43020667baa1562832ed92c42c080c7f48bce05b
Pivok <pivoc@protonmail.com>
2026-08-27 11:54:22 +0200

    Fix zig and c --name

--- templates/zig/lib/build.zig
+++ templates/zig/lib/build.zig
@@ -4,8 +4,8 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const mod = b.addModule("root", .{
-        .root_source_file = b.path("src/#PROJ_NAME#.zig"),
+    const mod = b.addModule("#PROJ_NAME#", .{
+        .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
